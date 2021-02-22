bb02a73d0e361cd30efc2383bee1652094a22ffc
dingeHCHS <69469881+dingeHCHS@users.noreply.github.com>
2021-02-22 11:37:32 -0600

    sum

---
+++ data.js
@@ -0,0 +1,55 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-criminallaw",
+      "name": "CriminalLaw",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -3.0807503982205215,
+        "pitch": 0.05212752689024214,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 2.714767461497516,
+          "pitch": 0.012717726319282363,
+          "title": "Introduction",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.6688989514430386,
+          "pitch": -0.0010226409082427779,
+          "title": "Mock Trial",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
